8c4e3773b1aa26e695063ec0a1b34427ed6fb94b
stonebox1122 <287779438@qq.com>
2019-07-03 22:48:33 +0800

    Initial commit

---
+++ src/main/java/com/stone/shiro/realms/ShiroRealm.java
@@ -0,0 +1,28 @@
+package com.stone.shiro.realms;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+/**
+ * @author stone
+ * @date 2019/7/3 21:23
+ * description
+ */
+public class ShiroRealm implements Realm {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
